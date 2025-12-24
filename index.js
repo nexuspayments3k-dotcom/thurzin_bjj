071205c13bec2138a503e66d83a5520cfc0ea722
nexuspayments3k-dotcom <nexuspayments3k@gmail.com>
2025-12-24 00:30:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+package.json
+{
+  "name": "instagram-discord-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.15.3",
+    "rss-parser": "^3.13.0",
+    "dotenv": "^16.4.5"
+  }
+}
